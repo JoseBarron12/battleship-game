966fe9c27cb4523688b26b91c4ff0ff2212baab9
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-11-17 22:05:54 -0600

    feat: add placed parameter to Ship class
    Allow gameboard to keep track if ship has been
    placed to aid in its methods for ship placement.

--- src/ship.js
+++ src/ship.js
@@ -6,11 +6,16 @@ export class Ship {
         this._length = length;
         this._hits = 0;
         this._sunk = false;
+        this._placed = false;
     }
 
     get length() { return this._length; }
+   
     get hits() { return this._hits; }
     set hits(newHits) { this._hits = newHits;}
+
+    get placed() { return this._placed; }
+    set placed(newFlag) { this._placed = newFlag; }
 
     hit() { this.hits = this.hits + 1; }
     isSunk() { return this.length == this.hits }
