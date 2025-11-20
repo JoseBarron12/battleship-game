f86303e7df7a5312c3376c8117c0bc28a4e9e0fb
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-11-20 15:56:56 -0600

    refactor: move logic for retrieval of user input
    Logic move to a new function for reusability in
    future implementations.

--- src/functionality.js
+++ src/functionality.js
@@ -1,5 +1,32 @@
 import { display } from "./display";
 
+const getShipInput = (form) => {
+    const fields = form.querySelectorAll("fieldset");
+            
+    const arrOfShips = [];
+            
+    fields.forEach(field => {
+        const selects = field.querySelectorAll("select");
+                
+        const shipLabel = field.querySelector("label").getAttribute("for");
+        const shipType = shipLabel.slice(0,shipLabel.indexOf("-"));
+
+        const shipObj = {
+            shipName: shipType
+        };
+                
+        selects.forEach(select => {
+            const id = select.getAttribute("id");
+            const input = select.value;
+            const shipPlay = id.slice(id.indexOf("-") + 1);
+            shipObj[shipPlay] = input;
+
+        });
+            arrOfShips.push(shipObj);
+        });
+    return arrOfShips;
+}
+
 export const functionality = (function() {
     const startGameBtn = (btn, parent) => {
         btn.addEventListener("click", () => {
@@ -12,30 +39,8 @@ export const functionality = (function() {
     const submitShipBtn = (btn, form) => {
         btn.addEventListener("click", (e) => {
             e.preventDefault();
-
-            const fields = form.querySelectorAll("fieldset");
-            
-            const arrOfShips = [];
-            
-            fields.forEach(field => {
-                const selects = field.querySelectorAll("select");
-                
-                const shipLabel = field.querySelector("label").getAttribute("for");
-                const shipType = shipLabel.slice(0,shipLabel.indexOf("-"));
-
-                const shipObj = {
-                    shipName: shipType
-                };
-                
-                selects.forEach(select => {
-                    const id = select.getAttribute("id");
-                    const input = select.value;
-                    const shipPlay = id.slice(id.indexOf("-") + 1);
-                    shipObj[shipPlay] = input;
-
-                });
-                arrOfShips.push(shipObj);
-            });
+  
+            const arrOfShips = getShipInput(form);
 
             console.log(arrOfShips);
         })
