dfafb3e67287882989a57e3a66237fd28653c8e7
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-11-20 00:47:18 -0600

    feat: add styling to initial game screen.
    Screen no longer has default styling and positioning.
    Initial game screen fits battleship ui look.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { display } from "./display"
 import "./styles.css"
 
 display.gameboard(document.querySelector(".game-board"));
-//display.initialScreen(document.querySelector(".opponent-board"));
-display.gameboard(document.querySelector(".opponent-board"))
+display.initialScreen(document.querySelector(".opponent-board"));
+//display.gameboard(document.querySelector(".opponent-board"), true)
 
 display.gamePieces(document.querySelector(".hit-pieces"),true);
 display.gamePieces(document.querySelector(".miss-pieces"), false);
