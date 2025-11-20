ce040663e016aa6525788705e34b23e6c5912391
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-11-20 00:24:57 -0600

    refactor: change call of funcs

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import { display } from "./display"
 import "./styles.css"
 
 display.gameboard(document.querySelector(".game-board"));
-display.gameboard(document.querySelector(".board"))
+//display.initialScreen(document.querySelector(".opponent-board"));
+display.gameboard(document.querySelector(".opponent-board"))
 
 display.gamePieces(document.querySelector(".hit-pieces"),true);
 display.gamePieces(document.querySelector(".miss-pieces"), false);
 
 const hitPieces = document.querySelector(".hit-pieces");
 console.log(hitPieces.children.length);
-
 
 const missPieces = document.querySelector(".miss-pieces");
 console.log(missPieces.children.length);
